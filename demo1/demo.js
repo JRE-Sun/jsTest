21167447cbf4ad5a704b4f7f62ec0ba472c1c54c
JRE-Sun <jresun@163.com>
2017-04-03 17:50:44 +0800

    今日头条

--- demo1/demo.js
+++ demo1/demo.js
@@ -1,6 +1,12 @@
 window.onload = function() {
     var input = document.querySelector("input");
-    var div = document.querySelector("div");
+    var body = document.querySelector("body");
+    var div = document.createElement("div");
+    var datalist = document.createElement("datalist");
+    datalist.setAttribute("id", "pasta");
+    body.appendChild(datalist);
+    body.appendChild(div);
+    var index = 0;
     //监听回车
     document.addEventListener("keydown", function(e) {
         var e = e || window.event;
@@ -8,18 +14,32 @@ window.onload = function() {
         if (code == 13) {
             if (input.value) {
                 // input的值要用value获取，不能用innhtml
+                var option = document.createElement("option");
                 var p = document.createElement("p");
+                option.index = index;
+                p.index = index;
+                index++;
                 p.addEventListener("click", function() {
                     // alert(this.innerHTML);
-                    this.parentElement.removeChild(this);
+                    var that = this;
+                    var options = document.querySelectorAll("option");
+                    var ps = document.querySelectorAll("p");
+                    for (var i = 0; i < ps.length; i++) {
+                        // alert(options[i].index);
+                        if (ps[i] == that) {
+                            datalist.removeChild(options[i]);
+                            div.removeChild(ps[i]);
+                        }
+                    }
                 }, false);
                 p.innerHTML = input.value;
-                div.appendChild(p);
+                option.innerHTML = input.value;
+                datalist.insertBefore(option, datalist.firstElementChild);
+                div.insertBefore(p, div.firstElementChild);
                 input.value = "";
             } else {
                 alert("填写错误！");
             }
-
         }
     }, false);
 };
